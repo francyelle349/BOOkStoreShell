03d64a5655111a7cf226e8d773e3b2c952b7b1ca
gabriel <gabrielbarrosguimaraes@live.com>
2023-06-15 12:56:46 -0400

    Criando o singleton de conexão

---
+++ Model/dbConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Model
+{
+    public sealed class dbConnection
+    {
+        private static volatile SqlConnection instance;
+        private dbConnection()
+        {
+
+        }
+
+        public static SqlConnection DB_Connection
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SqlConnection(ConfigurationManager.ConnectionStrings["CS_ADO_NET"].ConnectionString);
+                }
+                return instance;
+            }
+        }
+    }
+}
